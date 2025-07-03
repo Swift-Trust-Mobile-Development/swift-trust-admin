b8734f9bb39e8db8ee5842b4468016b872f6af58
@exeProcess <ajani_habeeb88@yahoo.com>
2025-07-03 04:46:03 +0100

    publish

--- dist/pages/forms/script.js
+++ dist/pages/forms/script.js
@@ -1,12 +1,12 @@
-document.getElementById("loginForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  const email = document.getElementById("email").value;
-  alert("Login attempted for: " + email);
-});
+// document.getElementById("loginForm").addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   const email = document.getElementById("email").value;
+//   alert("Login attempted for: " + email);
+// });
 
-document.getElementById("forgotPasswordForm")?.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const email = document.getElementById("email").value;
-  alert("Password reset link sent to: " + email);
-});
+// document.getElementById("forgotPasswordForm")?.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   const email = document.getElementById("email").value;
+//   alert("Password reset link sent to: " + email);
+// });
 
